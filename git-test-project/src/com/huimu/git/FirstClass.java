5062ed1f5ae6dd6fa9ef93e18214185112afd35d
lijin <lijinsdn@126.com>
2019-08-27 11:53:05 +0800

    修改本地git

--- git-test-project/src/com/huimu/git/FirstClass.java
+++ git-test-project/src/com/huimu/git/FirstClass.java
@@ -4,4 +4,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public static void test() {
+        System.out.println("hello git project");
+    }
 }
